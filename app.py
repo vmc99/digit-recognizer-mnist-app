4b5f34c0c999dcea6437c1e6a72bbea96f0edd61
Vamsi <mvamsi99@hotmail.com>
2021-07-15 18:53:20 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,121 @@
+import streamlit as st
+from streamlit_drawable_canvas import st_canvas
+import cv2
+import torch
+import torchvision.transforms as T
+import torch.nn.functional as F
+from model import transform_image, predict_image
+import pandas as pd
+import plotly.graph_objects as go
+import gc
+
+#Enable garbage collection
+gc.enable()
+
+
+st.set_page_config(page_title="Digit Recognizer App", initial_sidebar_state="expanded")
+st.title('Digit Recognizer')
+st.write("This is a simple image classification web app to **recognize the digit** drawn in the canvas.")
+st.markdown('### Draw a digit !')
+
+
+st.sidebar.header("Configuration")
+stroke_width = st.sidebar.slider("Brush width: ", 10, 30, 20)
+drawing_mode = st.sidebar.checkbox("Drawing mode ?", True)
+
+
+# To plot classes and probabilitites
+def plot_fig(df):
+
+    fig = go.Figure(go.Bar(
+    x=df[0].tolist(),
+    y=list(df.index),
+    orientation='h'))
+
+    fig.update_yaxes(type='category')
+
+    # Label axes and title, center the title, change fonts 
+    fig.update_layout(    
+        title={
+                'text': "Classes vs Probabilities",
+                'y':0.9,
+                'x':0.5,
+                'xanchor': 'center',
+                'yanchor': 'top'},
+        xaxis_title="Probabilities",
+        yaxis_title="Classes",
+        font=dict(
+            family="Courier New, monospace",
+            size=18,
+            # color="RebeccaPurple"
+            ))
+
+    st.plotly_chart(fig)
+
+
+SIZE = 256
+canvas_result = st_canvas(
+    fill_color='#000000',
+    stroke_width=stroke_width,
+    stroke_color='#FFFFFF',
+    background_color='#000000',
+    width=SIZE,
+    height=SIZE,
+    drawing_mode="freedraw" if drawing_mode else "transform",
+    key='canvas')
+
+if canvas_result.image_data is not None:
+
+    # cv2 METHOD
+    # nd_array -> resized nd_array -> grayscale nd_array -> pytorch tensor
+
+    # Resize the image to 28x28 for the model input
+    img = cv2.resize(canvas_result.image_data.astype('uint8'), (28, 28))
+    # Rescaling the image just to view the model input clearly 
+    rescaled = cv2.resize(img, (SIZE, SIZE), interpolation=cv2.INTER_NEAREST) 
+    st.write('`Model input (rescaled)`')
+    st.image(rescaled)
+
+
+    # Convert to grayscale
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Convert to Pytorch Tensor
+    tensor_img = transform_image(img_gray)
+
+
+    clicked = st.button('Run')
+
+    if clicked:
+
+        with st.spinner(text='In progress'):
+            image_transformed = transform_image(img_gray)
+            class_label , confidence, probs = predict_image(image_transformed)
+
+        st.info('Run Successful !')
+
+        if confidence > 50:
+            st.write('### `Prediction` : ', str(class_label))
+            st.write('### `Confidence` : {}%'.format(confidence)) 
+            st.write('&nbsp') # new line
+            st.write('**Probabilities**')
+            df = pd.DataFrame(probs.numpy())
+            st.dataframe(df)
+            df = df.transpose()
+            plot_fig(df)
+
+           
+
+            del img_gray, image_transformed , class_label, confidence, df 
+
+        else:
+            st.write('### `Prediction` : Unable to predict')
+            st.write('### `Confidence` : {}%'.format(confidence)) 
+            st.write('&nbsp') # new line
+            st.write('**Probabilities**')
+            df = pd.DataFrame(probs.numpy())
+            st.dataframe(df)
+            df = df.transpose()
+            plot_fig(df)
+            del img_gray, image_transformed , class_label, confidence, df
+
+gc.collect()
---
+++ model.py
@@ -0,0 +1,94 @@
+import torch
+import torchvision.transforms as T
+import torch.nn.functional as F
+import torch.nn as nn
+import math
+import gc
+import streamlit as st
+
+#Enable garbage collection
+gc.enable()
+
+def conv_block(in_channels, out_channels, pool=False):
+    layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1), 
+              nn.BatchNorm2d(out_channels), 
+              nn.ReLU(inplace=True)]
+    if pool: layers.append(nn.MaxPool2d(2))
+    return nn.Sequential(*layers)
+
+class ResNet8(nn.Module):
+    def __init__(self, in_channels, num_classes):
+        super().__init__()
+        # 1 x 28 x 28
+        self.conv1 = conv_block(in_channels, 64) # 64 x 28 x 28
+        self.conv2 = conv_block(64, 128, pool=True) # 128 x 14 x 14
+        self.res1 = nn.Sequential(conv_block(128, 128), 
+                                  conv_block(128, 128)) # 128 x 14 x 14
+        
+        self.conv3 = conv_block(128, 256, pool=True)  # 256 x 7 x 7
+        self.res2 = nn.Sequential(conv_block(256, 256), 
+                                  conv_block(256, 256)) # 256 x 7 x 7
+        
+        self.classifier = nn.Sequential(nn.MaxPool2d(7),  # 256 x 1 x 1 since maxpool with 7x7
+                                        nn.Flatten(),    # 256*1*1 
+                                        nn.Dropout(0.2),
+                                        nn.Linear(256, num_classes))
+        
+    def forward(self, xb):
+        out = self.conv1(xb)
+        out = self.conv2(out)
+        out = self.res1(out) + out
+        out = self.conv3(out)
+        out = self.res2(out) + out
+        out = self.classifier(out)
+        return out
+
+
+
+def transform_image(image):
+    stats = ((0.1307), (0.3081))
+    my_transforms = T.Compose([
+
+                        T.ToTensor(),
+                        T.Normalize(*stats)
+
+                        ])
+
+    return my_transforms(image)
+
+
+
+
+@st.cache
+def initiate_model():
+
+    # Initiate model
+    in_channels = 1
+    num_classes = 10
+    model = ResNet8(in_channels, num_classes)
+    device = torch.device('cpu')
+    PATH = 'mnist-resnet.pth'
+    model.load_state_dict(torch.load(PATH, map_location=device))
+    model.eval()
+
+    return model
+
+
+
+
+def predict_image(img):
+    
+    # Convert to a batch of 1
+    xb = img.unsqueeze(0)
+
+    model = initiate_model()
+
+    # Get predictions from model
+    yb = model(xb)
+    # apply softamx
+    yb_soft = F.softmax(yb, dim=1)
+    # Pick index with highest probability
+    confidence , preds  = torch.max(yb_soft, dim=1)
+    gc.collect()
+    # Retrieve the class label, confidence and probabilities of all classes using sigmoid 
+    return preds[0].item(), math.trunc(confidence.item()*100), F.sigmoid(yb).detach()
